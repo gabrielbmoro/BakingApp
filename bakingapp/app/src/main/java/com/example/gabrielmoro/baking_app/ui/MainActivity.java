a9b11c132e2ec7da64708a5ad2db36155b498257
Gabriel Moro <tido4410@gmail.com>
2018-12-01 14:49:52 -0200

    Necessary files to start the project

---
+++ bakingapp/app/src/main/java/com/example/gabrielmoro/baking_app/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.gabrielmoro.baking_app.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gabrielmoro.baking_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ bakingapp/app/src/main/java/com/example/gabrielmoro/baking_app/ui/recipe_detail_screen/recipe_step_detail_screen/RecipeStepDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.gabrielmoro.baking_app.ui.recipe_detail_screen.recipe_step_detail_screen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gabrielmoro.baking_app.R;
+
+public class RecipeStepDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_step_detail);
+    }
+}
